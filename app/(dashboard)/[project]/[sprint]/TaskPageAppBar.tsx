e5993b88590709f4e4658b70507e23d755fa1010
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-03-18 19:17:40 +0800

    task page app bar styling

--- app/(dashboard)/[project]/[sprint]/TaskPageAppBar.tsx
+++ app/(dashboard)/[project]/[sprint]/TaskPageAppBar.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -15,23 +16,22 @@ import '../../../styles/globals.css';
 export default function TaskPageAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color='inherit'>Login</Button>
-        </Toolbar>
-      </AppBar>
+      <Toolbar>
+        <IconButton
+          size='large'
+          edge='start'
+          color='inherit'
+          aria-label='menu'
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color='inherit'>Login</Button>
+      </Toolbar>
+      <Divider />
     </Box>
   );
 };
