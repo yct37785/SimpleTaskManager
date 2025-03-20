85d2701443cd769d3dda8a2f7317054078302772
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-03-21 04:37:33 +0800

    styled landing page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,36 @@
-"use client";
+'use client';
 
 // components
 import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
 
 /**
  * home dashboard
  */
 export default function Home() {
   return (
-    <Box sx={{ p: 1 }}>
-      <Typography variant='h5' gutterBottom>main page</Typography>
-    </Box>
+    <main>
+
+      <Box sx={{ position: 'absolute', top: '35%', left: '35%', maxWidth: 500 }}>
+        <Typography variant='h4' fontWeight='bold' color='primary' gutterBottom>
+          Welcome to Task Manager
+        </Typography>
+        <Typography variant='h6' color='text.secondary' sx={{ fontWeight: 500, textAlign: 'left' }}>
+          Get started by creating a <strong>project</strong> to organize your work efficiently.
+        </Typography>
+        <Typography variant='h6' color='text.secondary' sx={{ fontWeight: 500, textAlign: 'left', mt: 2 }}>
+          Within each project, add <strong>sprints</strong> to break down tasks and track progress.
+        </Typography>
+      </Box>
+
+      <Box sx={{ position: 'absolute', top: '7%', left: '25%', display: 'flex', alignItems: 'center' }}>
+        <Image src='/arrow.svg' alt='Arrow pointing to sidebar' width={100} height={100} 
+        style={{ transform: 'rotate(20deg)' }} />
+        <Typography variant='body1' fontWeight='bold' color='text.secondary' sx={{ mt: 5, ml: 2 }}>
+          Click <strong>'Add Project'</strong> in the sidebar!
+        </Typography>
+      </Box>
+
+    </main>
   );
 };
