8eb17bbbb9f865ce583b12bf65c62bff10870a19
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-03-24 10:11:43 +0800

    columns start with min height and have extra visual space when expanding

--- app/components/TaskColumn/TaskColumn.tsx
+++ app/components/TaskColumn/TaskColumn.tsx
@@ -8,7 +8,7 @@ import {
   Add as AddIcon
 } from '@mui/icons-material';
 // types
-import { Column } from '@defines/schemas';
+import { Column, ColumnType } from '@defines/schemas';
 import { TASK_COLUMN_WIDTH, TASK_COLUMN_MIN_HEIGHT } from '@defines/consts';
 
 type Props = {
@@ -23,10 +23,15 @@ export default function TaskColumn({ column, setOpenColumn }: Props) {
   return (
     <Box>
       <Typography variant='body2'>{column.title}</Typography>
-      <Box sx={{ border: '2px dashed #ccc', width: TASK_COLUMN_WIDTH, borderRadius: 2, p: 2, flex: 1, mt: 1, textAlign: 'center' }}>
+      <Box sx={{
+        border: '2px dashed #ccc', width: TASK_COLUMN_WIDTH, borderRadius: 2, p: 2, flex: 1,
+        mt: 1, textAlign: 'center', display: 'flex', flexDirection: 'column', minHeight: 500
+      }}>
         <Droppable droppableId={column.id}>
           {(provided) => (
-            <Box ref={provided.innerRef} {...provided.droppableProps}>
+            <Box ref={provided.innerRef} {...provided.droppableProps}
+              sx={{ display: 'flex', flexDirection: 'column', minHeight: 400, pb: 50 }}
+            >
               {column.tasks.map((task, index) => (
                 <TaskCard key={task.id} task={task} index={index} />
               ))}
@@ -34,8 +39,8 @@ export default function TaskColumn({ column, setOpenColumn }: Props) {
             </Box>
           )}
         </Droppable>
-        <Button variant='outlined' startIcon={<AddIcon />}
-          onClick={() => setOpenColumn(column.id)}>new task</Button>
+        {column.type === ColumnType.TODO ? <Button variant='outlined' startIcon={<AddIcon />}
+          onClick={() => setOpenColumn(column.id)}>new task</Button> : null}
       </Box>
     </Box>
   );
