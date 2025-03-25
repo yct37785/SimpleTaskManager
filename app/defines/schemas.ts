696fd8441e5394df61148fa99168759435503c32
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-03-26 02:23:23 +0800

    new schema: workspace -> project -> sprint -> column -> task

--- app/defines/schemas.ts
+++ app/defines/schemas.ts
@@ -3,30 +3,64 @@ export type Label = {
   color: string;
 }
 
+/**
+ * task
+ * - eg. review chatper 1
+ */
 export type Task = {
   id: string;
   title: string;
   description: string;
-  dueDate: string; // ISO string
+  addedDate: number;
+  completedDate: number;
   labels?: Label[];
-  addDate: string; // ISO string for when the task was added
 }
 
+/**
+ * hosts tasks
+ * - eg. TODO/WIP/DONE
+ * - draggable ordered
+ */
 export type Column = {
   id: string;
-  isTodo: boolean;
   title: string;
+  isTodo: boolean;
   tasks: Task[];
 }
 
+/**
+ * sprint board with columns to sort tasks by TODO, WIP and DONE
+ * - eg. review algebra 1
+ * - ordered
+ */
 export type Sprint = {
   id: string;
   title: string;
+  startDate: number;
+  endDate: number;
   columns: Column[];
 }
 
+/**
+ * a project is split into multiple sprints spanning the set duration
+ * - eg. review math
+ * - sprints ordered chronogically
+ */
 export type Project = {
   id: string;
   title: string;
-  sprints: Record<string, Sprint>;
+  startDate: number;
+  endDate: number;
+  sprints: Sprint[];
+}
+
+/**
+ * a group of projects of the same category
+ * - eg. study for midterms
+ * - projects are unordered
+ */
+export type Workspace = {
+  id: string;
+  title: string;
+  projects: Record<string, Project>;
 }
