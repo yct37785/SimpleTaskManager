46d8929606158e6fd3079b83508cc3fc2382cbdb
yct12321 <yct37785@gmail.com>
2025-05-14 02:25:33 +0800

    project details bar height to at least fit one line of description

--- app/styles/dimens.ts
+++ app/styles/dimens.ts
@@ -4,4 +4,4 @@ export const appbar_height = 48;
 export const calendar_picker_height = 360;
 export const task_column_width = 250;
 export const task_column_min_height = 400;
-export const project_details_bar_height = 130;
+export const project_details_bar_height = 134;
